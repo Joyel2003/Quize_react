af7ea298f7196166519bec25c2affb0dbdf70892
Joyel <joyelmathew806@gmail.com>
2025-04-16 23:03:45 +0530

    Updated

---
+++ src/Components/QuestionBank.jsx
@@ -0,0 +1,22 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What is the capital of Haryana?",
+        options: ["Yamunanagar", "Panipat", "Gurgaon", "Chandigarh"],
+        answer: "Chandugarh",
+    },
+    {
+        id: 2,
+        question: "Which is the most heat absorving color?",
+        options: ["Red", "Orange", "White", "Black"],
+        answer:"White",
+    },
+    {
+        id: 3,
+        question: "What is the capital of India?",
+        options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+        answer: "Delhi"
+    }
+]
+
+export default qBank;
